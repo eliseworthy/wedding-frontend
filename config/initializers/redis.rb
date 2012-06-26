11a8529fba3e7ecea5ee34fe803e0cb5139293c0
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-25 23:14:31 -0400

    mo redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
 uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
 
 Dir["#{Rails.root}/app/mailers/*.rb"].each { |file| require file }
