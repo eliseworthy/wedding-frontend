ed0c4fb6bc95e1792baa133e15e3ed9d640bd9ea
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-25 22:48:49 -0400

    fixed spacing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+Dir["#{Rails.root}/app/mailers/*.rb"].each { |file| require file }
