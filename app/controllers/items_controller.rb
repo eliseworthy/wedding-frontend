eebe361da761ddabab2af0f95e4e8c122556737c
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-27 11:24:11 -0400

    authorization for creation and update of items fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,18 @@
 class ItemsController < ApplicationController
   def create
-    @wedding = WeddingRequest.find(params[:wedding_id])
+    @wedding = WeddingRequest.find(params[:item][:wedding_id])
     if @wedding.user_id == current_user.id
       params[:item][:wedding_id] = @wedding.id
       @item = ItemRequest.create(params[:item])
       if @item.success?
-        redirect_to user_wedding_path(current_user.id, @wedding.id), notice: "Successfully created item!"
+        redirect_to wedding_path(@wedding.id), notice: "Successfully created item!"
       else
         @items = ItemRequest.find_all_by_wedding(@wedding.id)
-        render "/weddings/show"
+        render "/weddings/show", notice: "Unable to save item."
       end
     else
-      render "/weddings/show", notice: "no"
+      @items = ItemRequest.find_all_by_wedding(@wedding.id)
+      render "/weddings/show", notice: "Unauthorized to create items for this wedding"
     end
   end
 
@@ -20,12 +21,18 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = ItemRequest.update(params[:id], params[:item])
-    @wedding_id = params[:item][:wedding_id]
-    if @item.success?
-      redirect_to wedding_path(@wedding_id), notice: "Successfully updated item!"
+    @wedding = WeddingRequest.find(params[:item][:wedding_id])
+    if @wedding.user_id == current_user.id
+      @item = ItemRequest.update(params[:id], params[:item])
+      @wedding_id = params[:item][:wedding_id]
+      if @item.success?
+        redirect_to wedding_path(@wedding_id), notice: "Successfully updated item!"
+      else
+        redirect_to wedding_path(@wedding_id), notice: "Couldn't update"
+      end
     else
-      redirect_to wedding_path(@wedding_id), notice: "Couldn't update"
+      @items = ItemRequest.find_all_by_wedding(@wedding.id)
+      render "/weddings/show", notice: "Unauthorized to create items for this wedding"
     end
   end
 end
