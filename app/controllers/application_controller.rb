e2fa5f60e92aed3c31214e31a62876edb97a1c93
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-30 17:30:12 -0400

    footer, side nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :recent_weddings, except: [:create, :update]
 
   def authorize
     redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
+
+  def recent_weddings
+    @recent_weddings = WeddingRequest.find_all.reverse.first(3)
   end
 
   private
@@ -10,4 +15,5 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
 end
