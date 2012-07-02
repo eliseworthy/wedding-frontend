c51d2066e50fc8ebddd14da494e688f3d5897c96
Elise Worthy <elise.worthy@livingsocial.com>
2012-07-02 15:57:29 -0400

    mailer

--- config/initializers/resque_mailer.rb
+++ config/initializers/resque_mailer.rb
@@ -1,11 +1,9 @@
-if Rails.env.production?
-	ActionMailer::Base.smtp_settings = {
-	  address: "smtp.gmail.com",
-	  port: 587,
-	  authentication: :plain,
-	  domain: 'herokuapp.com',
-	  user_name: ENV["EMAIL"],
-	  password: ENV["PASSWORD"]
-	  }
-  ActionMailer::Base.delivery_method = :smtp
-end
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  authentication: :plain,
+  domain: 'herokuapp.com',
+  user_name: ENV["EMAIL"],
+  password: ENV["PASSWORD"]
+  }
+ActionMailer::Base.delivery_method = :smtp
