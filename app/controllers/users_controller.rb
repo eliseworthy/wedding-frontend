14178902bf7374948f5d8af0b0d32f2dfa2342de
Elise Worthy <elise.worthy@livingsocial.com>
2012-07-02 19:18:15 -0400

    updated emails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      UserMailer.signup_confirmation(@user.email).deliver
+      UserMailer.signup_confirmation(@user.name, @user.email).deliver
       UserKeyRequest.save_key(@user.api_key)
       flash[:notice] = "Thanks, #{current_user.name}! Your account was successfully created!"
       redirect_to user_weddings_path(current_user.id)
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,15 @@
 class UserMailer < ActionMailer::Base
   include Resque::Mailer
-  default from: "terrorize@bridezil.la"
+  default from: "admin@bridled.org"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.signup_confirmation.subject
   #
-  def signup_confirmation(user_email)
+  def signup_confirmation(user_name, user_email)
+    @user_name = user_name
     @user_email = user_email
-    mail to: user_email, subject: "Dum dum de dum... Bridezilla awaits!"
+    mail to: user_email, subject: "Dum dum de dum... get started with Bridled!"
   end
 end
