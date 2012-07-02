7ad89deccfff45ab9d96a3c7dacbca6a5bd4d6a3
Elise Worthy <elise.worthy@livingsocial.com>
2012-07-01 23:46:59 -0400

    resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,4 @@
 require 'resque'
 
-if Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
