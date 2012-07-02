84a54fa30754d092016f91182fa697617c4017b7
Elise Worthy <elise.worthy@livingsocial.com>
2012-07-02 10:01:39 -0400

    validation for signup, clearer signup/login messaging

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :email, :password, :password_confirmation, :name
 
+  validates_presence_of :email, :name, :password_confirmation
   validates_uniqueness_of :email, :name
 end
